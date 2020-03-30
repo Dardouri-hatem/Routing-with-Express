aeffd020140e19950a27939aa8ad3753398ac572
Dardouri-hatem <dardourihatem@gmail.com>
2020-03-30 17:52:51 +0200

    Use app.get

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require ('express');
+const app=express();
 
-const app=express()
+const hour = new Date().getHours();
+
 app.use(date=(req,res,next)=>{
-let hour = new Date().getHours()
-let minute = new Date().getMinutes()
 if(hour>=17 || hour<08)
-res.send("Our office is not open now")
+res.send("<h1>Our office is not open now</h1>")
 else
-next()
+next();
 })
 
-app.use(express.static(__dirname+'/public'))
+app.get("/",(req,res)=>{
+    if(hour>=17 || hour<08)
+    res.send("<h1>Our office is not open now</h1>")
+    else {
+           res.sendFile(__dirname+"/public/home.html")
+    }
+})
+
+app.use(express.static(__dirname+'/public'));
+
 
 
 
 app.listen(3000,(err)=>{
-    if(err) console.log('server not running');
-    else console.log('server is running on port 3000...')
+    if(err) console.log('Server is not running');
+    else console.log('Server is running on port 3000...')
 })
